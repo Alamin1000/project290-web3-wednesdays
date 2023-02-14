64facf945b41694ad58b9575b3a011f8c8aba7fa
alamin <alaminhosen44000@gmail.com>
2023-02-15 01:38:01 +0600

    a

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+  "use strict";
+})(jQuery);
+
+$(document).ready(function () {
+  // scroll up
+  $(function () {
+    $.scrollUp();
+  });
+
+  // preloader
+  $("#preloader").fadeOut(500);
+});
